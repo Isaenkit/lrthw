7b83456b950ba881523b79fe1b6909861a73d791
isaenkit <isabelle.gautier@orange.fr>
2016-06-20 16:31:06 +0200

    envoi des exercices

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Max'
+my_age = 40 # not a lie in 2009
+my_height = 75 # inches
+my_weight = 150 # lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Red'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
---
+++ ex6.rb
@@ -0,0 +1,25 @@
+types_of_people = 10
+x = "There are #{types_of_people} types of people."
+#première variable#
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+#seconde variable#
+
+puts x
+puts y
+
+puts "I said: #{x}."
+#reprise de la première variable dans un string.
+puts "I also said: '#{y}'."
+#reprise de la seconde variable dans un string
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
